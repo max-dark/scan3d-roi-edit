5c29f53808e61f9e154ddfb7ba23fffdad75cf20
Max Dark <maxim.dark@gmail.com>
2020-11-23 21:25:20 +0300

    add method stups for ROI model

--- scan/roi.py
+++ scan/roi.py
@@ -1,13 +1,55 @@
 # -*- coding: utf-8 -*-
 from typing import Dict, List
-from PyQt5.QtCore import QPointF, QRectF, QJsonDocument, QJsonValue
+from PyQt5.QtCore import QPointF, QRectF, QJsonDocument, QJsonValue, Qt
 from PyQt5.QtGui import QPolygonF
+
+from scan.types import Scan
 
 
 class SideRoi:
-    detect: List[QRectF]
+    support: List[QRectF]
     polygon: QPolygonF
+
+    def __init__(self):
+        self.support = list()
+        self.polygon = QPolygonF()
+
+    def in_support(self, point: QPointF) -> bool:
+        for s in self.support:
+            if s.contains(point):
+                return True
+
+        return False
+
+    def contains(self, point: QPointF) -> bool:
+        return self.polygon.containsPoint(point, Qt.OddEvenFill)
+
+    def to_json(self):
+        pass
+
+    def from_json(self):
+        return self
 
 
 class Roi:
     side: Dict[str, SideRoi]
+
+    def __init__(self):
+        self.side = dict()
+        for side in Scan.SIDES:
+            self.side[side] = SideRoi()
+
+    def left(self) -> SideRoi:
+        return self.side[Scan.SIDE_LEFT]
+
+    def top(self) -> SideRoi:
+        return self.side[Scan.SIDE_TOP]
+
+    def right(self) -> SideRoi:
+        return self.side[Scan.SIDE_RIGHT]
+
+    def to_json(self):
+        pass
+
+    def from_json(self):
+        return self
